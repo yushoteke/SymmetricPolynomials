339f709615a4e77eb01af25be45949193bbed2a6
david <yushoteke@gmail.com>
2020-09-14 15:07:33 -0400

    string doc for monomial

--- src/semi elementary monomial.jl
+++ src/semi elementary monomial.jl
@@ -1,14 +1,65 @@
 
 """
-A semi elementary monomial is the product of
-1.a non elementary symmetric polynomial with power 1
-2.an elementary monomial
+    semi_elementary_monomial(x,y)
 
-For example 3 * S(2,1,0,0) * e1^3 * e2 * e3 * e4^2
+First, given n variables, we say e_i is the elementary symmetric polynomial or order i with coefficient 1.\n
+For example,\n
+    n=3, e1 = x+y+z,    e2 = xy+xz+yz,          e3 = xyz
+    n=4, e1 = w+x+y+z,  e2 = wx+wy+wz+xy+xz+yz, e3 = wxy+wxz+wyz+xyz, e4 = wxyz
+    ...
+    and so on
 
-The non elementary term is always sorted
+Second, S(a1,a2...an) is the symmetric polynomial in n variables with powers 0 <= a1 <= a2...<= an and coefficient 1\n
+For example,\n
+    S(0,1,2) = x^2y + x^2z + y^2z
+    S(0,0,1,2) = w^2x + w^2y + w^2z + x^2y + x^2z + y^2z
+    S(0,0,0,0,4) = v^5 + w^5 + x^5 + y^5 + z^5
+    ...
+    and so on
+
+A semi elementary monomial is the product of\n
+    1.a non elementary symmetric polynomial with power 1
+    2.an elementary monomial
+
+For example,\n
+    S(0,0,3) * e1^3 * e2^1 * e3^0            = (x^3 + y^3 + z^3) * (x + y + z)^3 * (xy + xz + yz)
+    S(0,0,1,2) * e1^3 * e2^0 * e3^1 * e4^2   = (w^2x + w^2y + w^2z + x^2y + x^2z + y^2z) * (w + x + y + z)^3 * (wxy + wxz + wyz + xyz) * (wxyz)^2
+
+These would be represented as two tuples, for example\n
+    S(0,0,3) * e1^3 * e2^1 * e3^0           => (0,0,3),(3,1,0)
+    S(0,0,1,2) * e1^3 * e2^0 * e3^1 * e4^2  => (0,0,1,2),(3,0,1,2)
+
+This is where the name semi comes from, in that nearly all terms in the monomial are elementary except 1.
+
+When the user wants to create a semi elementary monomial, he or she needs to provide two tuples with non-negative integers.
+The first tuple will automatically be sorted by the constructor, the second will be provided as it is.
+
+    semi_elementary_monomial((0,2,1),(3,4,5))
+
+returns the same object as
+
+    semi_elementary_monomial((0,1,2),(3,4,5))
+
+Besides sorting, some trivial simplifications will also be made.
+First, e_n terms will be automatically pulled out, for example
+
+    semi_elementary_monomial((2,3,4),(0,0,0))
+
+will return
+
+    semi_elementary_monomial((0,1,2),(0,0,2))
+
+Which is basically saying
+    (x^2y^3z^4 + x^2z^3y^4 + y^2x^3z^4 + y^2z^3x^4 + z^2x^3y^4 + z^2x^4y^3) = (x^2y + x^2z + y^2z) * (xyz)^2
+
+Second, if the first term is already elementary, it will be merged into the second term, for example
+
+    semi_elementary_monomial((0,1,1),(0,0,0))
+
+will return
+
+    semi_elementary_monomial((0,0,0),(0,1,0))
 """
-
 struct semi_elementary_monomial{N}
     sp_term::NTuple{N,Int64}
     powers::NTuple{N,Int64}
@@ -29,12 +80,7 @@ struct semi_elementary_monomial{N}
 end
 
 dim(x::semi_elementary_monomial{N}) where {N} = N
-
 is_elementary(x::semi_elementary_monomial) = x.sp_term[end]==0
-
-#assumes y is elementary, returns the product
-mul_unsafe(x::semi_elementary_monomial{N},y::semi_elementary_monomial{N}) where {N} = semi_elementary_monomial(x.sp_term,x.powers.+y.powers)
-
 function Base.isless(x::semi_elementary_monomial{N},y::semi_elementary_monomial{N}) where {N}
     for i=N:-1:1
         x.sp_term[i] != y.sp_term[i] && return x.sp_term[i] < y.sp_term[i]
